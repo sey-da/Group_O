c63fc3ceba683f462bfb7eacc5a859de7b39effc
msusanrocha <m.susan.rocha@gmail.com>
2026-02-25 22:30:54 +0000

    Updated main.py to include necessary imports and names for the new environment data class

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from app.data_handler import download_datasets
+from app.environment_data import EnvironmentData
 
 files = download_datasets()
 print(files)
+
+
+data = EnvironmentData()
+maps = data.list_available_maps()
+
+for name, gdf in maps.items():
+    print(name, "→", len(gdf), "rows")
+    
